45e8dd9743164bf47d8c426d5894f8275536b181
Juan Cruz Viotti <juanchiviotti@gmail.com>
2015-06-22 08:40:05 -0400

    Make use of resin-device-logs

--- build/logs.js
+++ build/logs.js
@@ -4,15 +4,11 @@
  */
 
 (function() {
-  var PubNub, _, async, configModel, deviceModel, errors;
+  var Promise, configModel, deviceModel, logs;
 
-  async = require('async');
+  Promise = require('bluebird');
 
-  _ = require('lodash-contrib');
-
-  PubNub = require('pubnub');
-
-  errors = require('resin-errors');
+  logs = require('resin-device-logs');
 
   configModel = require('./models/config');
 
@@ -20,98 +16,63 @@
 
 
   /**
-   * subscribe callback
-   * @callback module:resin.logs~subscribeCallback
-   * @param {(Error|null)} error - error
-   * @param {String|String[]} message - log message
+   * @summary Subscribe to device logs
+   * @function
+   * @public
+   *
+   * @description
+   * The `logs` object yielded by this function emits the following events:
+   *
+   * - `line`: when a log line is received.
+   * - `error`: when an error happens.
+   *
+   * @param {String} deviceName - device name
+   * @param {Function} callback - callback (error, logs)
+   *
+   * @todo
+   * We should consider making this a readable stream.
+   *
+   * @example
+   * resin.logs.subscribe 'MyDevice', (error, logs) ->
+   *		throw error if error?
+   *
+   *		logs.on 'line', (line) ->
+   *			console.log(line)
    */
+
+  exports.subscribe = function(deviceName, callback) {
+    return Promise.props({
+      uuid: deviceModel.get(deviceName).get('uuid'),
+      pubNubKeys: configModel.getPubNubKeys()
+    }).then(function(results) {
+      return logs.subscribe(results.pubNubKeys, results.uuid);
+    }).nodeify(callback);
+  };
 
 
   /**
-   * @summary Subscribe to device logs by their UUID
-   * @public
+   * @summary Get device logs history
    * @function
+   * @public
    *
-   * @param {String} uuid - the device uuid
-   * @param {Object} options - logs options (history=0, tail=false)
-   * @param {module:resin.logs~subscribeCallback} callback - callback
-   *
-   * @throws {Error} Will throw if `options.history` is not a number or parseable string.
-   *
-   * @todo Find a way to test this
+   * @param {String} deviceName - device name
+   * @param {Function} callback - callback (error, history)
    *
    * @example
-   * uuid = 23c73a12e3527df55c60b9ce647640c1b7da1b32d71e6a39849ac0f00db828
-   * resin.logs.subscribe uuid, {
-   *		history: 20
-   * }, (error, message) ->
+   * resin.logs.history 'MyDevice', (error, history) ->
    *		throw error if error?
-   *		console.log(message)
    *
-   * @example
-   * uuid = 23c73a12e3527df55c60b9ce647640c1b7da1b32d71e6a39849ac0f00db828
-   * resin.logs.subscribe uuid, {
-   *		tail: true
-   * }, (error, message) ->
-   *		throw error if error?
-   *		console.log(message)
+   *		for line in history
+   *			console.log(line)
    */
 
-  exports.subscribe = function(uuid, options, callback) {
-    if (options == null) {
-      options = {};
-    }
-    _.defaults(options, {
-      history: 0,
-      tail: false
-    });
-    if (!_.isNumber(options.history)) {
-      return callback(new errors.ResinInvalidOption('history', options.history));
-    }
-    return deviceModel.isValidUUID(uuid, function(error, isValidUUID) {
-      if (error != null) {
-        return callback(error);
-      }
-      if (!isValidUUID) {
-        return callback(new Error("Invalid uuid: " + uuid));
-      }
-      return configModel.getPubNubKeys(function(error, pubnubKeys) {
-        var channel, pubnub;
-        if (error != null) {
-          return callback(error);
-        }
-        pubnubKeys.ssl = true;
-        pubnub = PubNub.init(pubnubKeys);
-        channel = "device-" + uuid + "-logs";
-        return pubnub.subscribe({
-          channel: channel,
-          callback: function(message) {
-            if (!options.tail) {
-              return;
-            }
-            return callback(null, message);
-          },
-          error: _.unary(callback),
-          connect: function() {
-            return pubnub.history({
-              count: options.history,
-              channel: channel,
-              error: _.unary(callback),
-              callback: function(message) {
-                if (options.tail) {
-                  return callback(null, _.first(message));
-                }
-                return pubnub.unsubscribe({
-                  channel: channel
-                }, function() {
-                  return callback(null, _.first(message));
-                });
-              }
-            });
-          }
-        });
-      });
-    });
+  exports.history = function(deviceName, callback) {
+    return Promise.props({
+      uuid: deviceModel.get(deviceName).get('uuid'),
+      pubNubKeys: configModel.getPubNubKeys()
+    }).then(function(results) {
+      return logs.history(results.pubNubKeys, results.uuid);
+    }).nodeify(callback);
   };
 
 }).call(this);
