0f0f9ea934589a1692b261ca4a1665471cfdcff4
Thodoris Greasidis <thgreasi@gmail.com>
2017-11-24 15:49:29 +0200

    Start using Typescript
    Connects-To: #353
    Change-Type: patch

---
+++ lib/util/device-types.ts
@@ -0,0 +1,25 @@
+// copied from the @resin-io/device-types
+// because that has the run-time dependency on coffee-script
+
+const find = <T>(array: T[], predicate: (el: T) => boolean) => {
+	for (const el of array) {
+		if (predicate(el)) {
+			return el;
+		}
+	}
+};
+
+const includes = <T>(array: T[], el: T) => array && array.indexOf(el) >= 0;
+
+const dtPredicate = (slug: string) => (deviceType: DeviceType) =>
+	deviceType.slug === slug || includes(deviceType.aliases, slug);
+
+export const findBySlug = (deviceTypes: DeviceType[], slug: string) => find(deviceTypes, dtPredicate(slug));
+
+export const normalizeDeviceType = (deviceTypes: DeviceType[], slug: string) => {
+	// returns `undefined` in case of invalid slug
+	const deviceType = findBySlug(deviceTypes, slug);
+	if (deviceType) {
+		return deviceType.slug;
+	}
+};
