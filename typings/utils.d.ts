bbc1389330ce189036ee0622aa2b386aa16496bb
Thodoris Greasidis <thgreasi@gmail.com>
2018-03-28 16:09:14 +0300

    Fully type the body of post & patch requests
    Also moves anything non model related to the pine
    typings file and re-exporting.
    
    Resolves: #480
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/utils.d.ts
+++ typings/utils.d.ts
@@ -1,3 +1,14 @@
+export type AnyObject = {
+	[index: string]: any;
+};
+
+export type PropsOfType<T, P> = {
+	[K in keyof T]: T[K] extends P ? K : never
+}[keyof T];
+
+// backwards compatible alternative for: Extract<keyof T, string>
+export type StringKeyof<T> = keyof T & string;
+
 // Functionally the same as Exclude, but for strings only.
 type Diff<T extends string, U extends string> = ({ [P in T]: P } &
 	{ [P in U]: never } & { [x: string]: never })[T];
