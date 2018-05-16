69738d4059a90998d3917341c27dd3ba881cd35c
Thodoris Greasidis <thgreasi@gmail.com>
2018-05-16 16:27:55 +0300

    Add transitional support for the name field on app & device env vars
    Resolves: #510
    Change-Type: patch

--- typings/resin-sdk.d.ts
+++ typings/resin-sdk.d.ts
@@ -545,10 +545,9 @@ declare namespace ResinSdk {
 		application: NavigationResource<Application>;
 	}
 
-	interface SecondaryEnvironmentVariableBase {
-		id: number;
-		env_var_name: string;
-		value: string;
+	interface SecondaryEnvironmentVariableBase extends EnvironmentVariableBase {
+		/** @deprecated */
+		env_var_name?: string;
 	}
 
 	interface ApplicationEnvironmentVariable
