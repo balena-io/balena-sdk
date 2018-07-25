e5c9b9090f511a4112c0843cb5db16714f21d436
Thodoris Greasidis <thgreasi@gmail.com>
2018-07-25 13:17:59 +0300

    typings: Update to be compatible with TS 2.9
    This is still compatible with previous versions on TS.
    Also moves some of the Pine related typings in the pine client file but still re-exports them to avoid a breaking change.
    
    Resolves: #550
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@resin.io>

--- typings/pinejs-client-core.d.ts
+++ typings/pinejs-client-core.d.ts
@@ -1,3 +1,27 @@
+// backwards compatible alternative for: Extract<keyof T, string>
+type StringKeyof<T> = keyof T & string;
+
+export interface WithId {
+	id: number;
+}
+
+export interface PineDeferred {
+	__id: number;
+}
+
+/**
+ * When not selected-out holds a deferred.
+ * When expanded hold an array with a single element.
+ */
+export type NavigationResource<T = WithId> = T[] | PineDeferred;
+
+/**
+ * When expanded holds an array, otherwise the property is not present.
+ * Selecting is not suggested,
+ * in that case it holds a deferred to the original resource.
+ */
+export type ReverseNavigationResource<T = WithId> = T[] | undefined;
+
 // based on https://github.com/resin-io/pinejs-client-js/blob/master/core.d.ts
 
 type RawFilter =
@@ -36,6 +60,37 @@ type FilterExpressions<T> = {
 
 type Filter<T> = ResourceObjFilter<T> & FilterExpressions<T>;
 
-type BaseExpandFor<T> = { [k in keyof T]?: object } | keyof T;
+type BaseExpandFor<T> = { [k in keyof T]?: object } | StringKeyof<T>;
 
 export type Expand<T> = BaseExpandFor<T> | Array<BaseExpandFor<T>>;
+
+export interface PineOptions {
+	$select?: string[] | string | '*';
+	$filter?: object;
+	$expand?: object | string;
+	$orderby?: OrderBy;
+	$top?: number;
+	$skip?: number;
+}
+
+export interface PineOptionsFor<T> extends PineOptions {
+	$select?: Array<StringKeyof<T>> | StringKeyof<T> | '*';
+	$filter?: Filter<T>;
+	$expand?: Expand<T>;
+}
+
+export interface PineParams {
+	resource: string;
+	id?: number;
+	body?: object;
+	options?: PineOptions;
+}
+
+export interface PineParamsFor<T> extends PineParams {
+	body?: Partial<T>;
+	options?: PineOptionsFor<T>;
+}
+
+export interface PineParamsWithIdFor<T> extends PineParamsFor<T> {
+	id: number;
+}
