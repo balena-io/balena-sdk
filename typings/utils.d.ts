22fe256f6af06e794499802c4a292bfef3aa9c2d
Thodoris Greasidis <thgreasi@gmail.com>
2019-02-25 13:37:51 +0200

    Type balena request send as a generic
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/utils.d.ts
+++ typings/utils.d.ts
@@ -9,11 +9,4 @@ export type PropsOfType<T, P> = {
 // backwards compatible alternative for: Extract<keyof T, string>
 export type StringKeyof<T> = keyof T & string;
 
-// Functionally the same as Exclude, but for strings only.
-type Diff<T extends string, U extends string> = ({ [P in T]: P } &
-	{ [P in U]: never } & { [x: string]: never })[T];
-
-export type Omit<T, K extends keyof T & string> = Pick<
-	T,
-	Diff<keyof T & string, K>
->;
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
