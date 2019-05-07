9a8001b9e7e727d5e6f4bf88cbc06501c610149f
Thodoris Greasidis <thgreasi@gmail.com>
2019-05-07 16:16:50 +0300

    typings: Add `is_public` to the application model
    Resolves: #676
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/balena-sdk.d.ts
+++ typings/balena-sdk.d.ts
@@ -274,6 +274,7 @@ declare namespace BalenaSdk {
 		has_dependent?: boolean;
 		is_accessible_by_support_until__date: string;
 		should_track_latest_release: boolean;
+		is_public: boolean;
 
 		application_type: NavigationResource<ApplicationType>;
 		depends_on__application: NavigationResource<Application>;
