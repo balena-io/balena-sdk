be5dd8471f86dc60bc09c1e17679602ecf4ffd21
Thodoris Greasidis <thgreasi@gmail.com>
2019-10-09 20:52:27 +0300

    typings: Add missing props in DeviceTypeJson
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/balena-sdk.d.ts
+++ typings/balena-sdk.d.ts
@@ -76,6 +76,7 @@ declare namespace BalenaSdk {
 		interface DeviceType {
 			slug: string;
 			name: string;
+			aliases: string[];
 
 			arch: string;
 			state?: string;
@@ -137,7 +138,7 @@ declare namespace BalenaSdk {
 			type: string;
 			min?: number;
 			choices?: string[] | number[];
-			choicesLabels?: { [key: string]: string };
+			choicesLabels?: Dictionary<string>;
 		}
 	}
 
@@ -1051,7 +1052,9 @@ declare namespace BalenaSdk {
 				getLocalIPAddressess(uuidOrId: string | number): Promise<string[]>;
 				getDashboardUrl(uuid: string): string;
 				getSupportedDeviceTypes(): Promise<string[]>;
-				getManifestBySlug(slugOrName: string): Promise<DeviceTypeJson.DeviceType>;
+				getManifestBySlug(
+					slugOrName: string,
+				): Promise<DeviceTypeJson.DeviceType>;
 				getManifestByApplication(
 					nameOrId: string | number,
 				): Promise<DeviceTypeJson.DeviceType>;
@@ -1251,7 +1254,12 @@ declare namespace BalenaSdk {
 				getDeviceTypes: () => Promise<DeviceTypeJson.DeviceType[]>;
 				getDeviceOptions(
 					deviceType: string,
-				): Promise<Array<DeviceTypeJson.DeviceTypeOptions | DeviceTypeJson.DeviceInitializationOptions>>;
+				): Promise<
+					Array<
+						| DeviceTypeJson.DeviceTypeOptions
+						| DeviceTypeJson.DeviceInitializationOptions
+					>
+				>;
 			};
 			image: {
 				get(id: number, options?: PineOptionsFor<Image>): Promise<Image>;
