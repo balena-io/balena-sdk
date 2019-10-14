d704c610c2ccd7a80df1e72111a51b3d87678411
Thodoris Greasidis <thgreasi@gmail.com>
2019-10-14 23:57:05 +0300

    auth: Add getPersonalOrganizationId() method
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/balena-sdk.d.ts
+++ typings/balena-sdk.d.ts
@@ -767,6 +767,7 @@ declare namespace BalenaSdk {
 			whoami: () => Promise<string | undefined>;
 			isLoggedIn: () => Promise<boolean>;
 			getUserId: () => Promise<number>;
+			getPersonalOrganizationId: () => Promise<number>;
 			getEmail: () => Promise<string>;
 
 			twoFactor: {
