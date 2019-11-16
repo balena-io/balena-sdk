face6db303c00c0b6c5855dbcfa2f69489093396
Thodoris Greasidis <thgreasi@gmail.com>
2019-11-16 17:04:06 +0200

    typings: Expose PineOptions WithSelect & WithFIlter variants
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>
    ODataOptionsWithFilter

--- typings/pinejs-client-core.d.ts
+++ typings/pinejs-client-core.d.ts
@@ -232,6 +232,9 @@ export interface ODataOptions<T> {
 export type ODataOptionsWithSelect<T> = ODataOptions<T> &
 	Required<Pick<ODataOptions<T>, '$select'>>;
 
+export type ODataOptionsWithFilter<T> = ODataOptions<T> &
+	Required<Pick<ODataOptions<T>, '$filter'>>;
+
 export type SubmitBody<T> = {
 	[k in keyof T]?: T[k] extends AssociatedResource ? number | null : T[k];
 };
@@ -259,7 +262,7 @@ export interface ParamsObjWithSelect<T> extends ParamsObj<T> {
 }
 
 export interface ParamsObjWithFilter<T> extends ParamsObj<T> {
-	options: ODataOptions<T> & Required<Pick<ODataOptions<T>, '$filter'>>;
+	options: ODataOptionsWithFilter<T>;
 }
 
 export interface UpsertParams<T>
