b555be36504bfaec19234dcee099f7af07e22247
Thodoris Greasidis <thgreasi@gmail.com>
2019-11-25 17:34:44 +0200

    typings: Add is_of__actor on the ApiKey
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/balena-sdk.d.ts
+++ typings/balena-sdk.d.ts
@@ -282,6 +282,8 @@ declare namespace BalenaSdk {
 		created_at: string;
 		name: string;
 		description: string | null;
+
+		is_of__actor: PineDeferred;
 	}
 
 	interface Application {
@@ -366,16 +368,16 @@ declare namespace BalenaSdk {
 		| null;
 
 	interface Release {
-		commit: string;
-		created_at: string;
 		id: number;
+		created_at: string;
+		commit: string;
 		composition: string | null;
+		status: ReleaseStatus;
 		source: string;
 		build_log: string | null;
-		end_timestamp: string;
 		start_timestamp: string;
-		status: ReleaseStatus;
 		update_timestamp: string | null;
+		end_timestamp: string;
 
 		is_created_by__user: OptionalNavigationResource<User>;
 		belongs_to__application: NavigationResource<Application>;
@@ -604,8 +606,8 @@ declare namespace BalenaSdk {
 	}
 
 	interface ServiceInstance {
-		created_at: string;
 		id: number;
+		created_at: string;
 		service_type: string;
 		ip_address: string;
 		last_heartbeat: string;
@@ -613,6 +615,7 @@ declare namespace BalenaSdk {
 
 	interface Service {
 		id: number;
+		created_at: string;
 		service_name: string;
 		application: NavigationResource<Application>;
 	}
