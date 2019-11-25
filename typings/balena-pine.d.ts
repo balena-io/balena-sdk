8e5f392933492d480c689bbff0a990ca0374d3c6
Thodoris Greasidis <thgreasi@gmail.com>
2019-11-25 19:58:47 +0200

    **BREAKING** Make $filter or id required in pine DELETE & PATCH
    Change-type: major
    See: https://github.com/balena-io/resin-api/pull/2226
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/balena-pine.d.ts
+++ typings/balena-pine.d.ts
@@ -5,14 +5,14 @@ import * as PineClient from './pinejs-client-core';
 declare namespace BalenaPine {
 	interface Pine {
 		delete<T>(
-			params: PineClient.ParamsObjWithId<T> | PineClient.ParamsObj<T>,
+			params: PineClient.ParamsObjWithId<T> | PineClient.ParamsObjWithFilter<T>,
 		): Promise<'OK'>;
 		get<T>(params: PineClient.ParamsObjWithId<T>): Promise<T>;
 		get<T>(params: PineClient.ParamsObj<T>): Promise<T[]>;
 		get<T, Result>(params: PineClient.ParamsObj<T>): Promise<Result>;
 		post<T>(params: PineClient.ParamsObj<T>): Promise<T>;
 		patch<T>(
-			params: PineClient.ParamsObjWithId<T> | PineClient.ParamsObj<T>,
+			params: PineClient.ParamsObjWithId<T> | PineClient.ParamsObjWithFilter<T>,
 		): Promise<'OK'>;
 		upsert<T>(params: PineClient.UpsertParams<T>): Promise<T | 'OK'>;
 	}
