5b9b0ca6a921f04d3b3820508f10d93ea01b859f
Thodoris Greasidis <thgreasi@gmail.com>
2020-01-29 20:22:01 +0200

    **BREAKING**  Change application and device commit to release references
    Resolves: #742
    Change-type: major
    See: https://github.com/balena-io/open-balena-api/pull/163
    See: https://github.com/balena-io/resin-api/pull/2100
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- typings/balena-sdk.d.ts
+++ typings/balena-sdk.d.ts
@@ -293,7 +293,6 @@ declare namespace BalenaSdk {
 		app_name: string;
 		device_type: string;
 		slug: string;
-		commit: string;
 		is_accessible_by_support_until__date: string;
 		is_host: boolean;
 		should_track_latest_release: boolean;
@@ -302,6 +301,7 @@ declare namespace BalenaSdk {
 		application_type: NavigationResource<ApplicationType>;
 		depends_on__application: NavigationResource<Application>;
 		organization: NavigationResource<Organization>;
+		should_be_running__release: NavigationResource<Release>;
 
 		application_config_variable: ReverseNavigationResource<ApplicationVariable>;
 		application_environment_variable: ReverseNavigationResource<
@@ -390,6 +390,9 @@ declare namespace BalenaSdk {
 			id: number;
 			image: NavigationResource<Image>;
 		}>;
+		should_be_running_on__application: ReverseNavigationResource<Application>;
+		is_running_on__device: ReverseNavigationResource<Device>;
+		should_be_running_on__device: ReverseNavigationResource<Device>;
 		release_tag: ReverseNavigationResource<ReleaseTag>;
 	}
 
@@ -530,7 +533,6 @@ declare namespace BalenaSdk {
 		is_connected_to_vpn: boolean;
 		is_in_local_mode?: boolean;
 		is_locked_until__date: string;
-		is_on__commit: string;
 		is_web_accessible: boolean;
 		is_active: boolean;
 		is_online: boolean;
@@ -559,6 +561,7 @@ declare namespace BalenaSdk {
 
 		belongs_to__application: NavigationResource<Application>;
 		belongs_to__user: NavigationResource<User>;
+		is_running__release: NavigationResource<Release>;
 		should_be_running__release: NavigationResource<Release>;
 		is_managed_by__service__instance: NavigationResource<ServiceInstance>;
 		is_managed_by__device: NavigationResource<Device>;
