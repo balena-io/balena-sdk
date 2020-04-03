fb4605c597ff8221b010ef100a66d21a66f18ff7
Thodoris Greasidis <thgreasi@gmail.com>
2020-04-03 21:46:06 +0300

    Use pine.upsert() for setting tags & envars
    Change-type: major
    Depends-on: https://github.com/balena-io/resin-api/pull/2018
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- lib/util/index.ts
+++ lib/util/index.ts
@@ -7,12 +7,6 @@ import * as Pine from '../../typings/pinejs-client-core';
 import { getOsUpdateHelper as updateHelper } from './device-actions/os-update';
 import * as dt from './device-types';
 
-export interface ErrorResponse {
-	code: string;
-	statusCode?: number;
-	body?: string;
-}
-
 export const getOsUpdateHelper = updateHelper;
 export const deviceTypes = dt;
 
@@ -80,14 +74,6 @@ export const isNoDeviceForKeyResponse = (err: Error) =>
 export const isNoApplicationForKeyResponse = (err: Error) =>
 	isBalenaRequestErrorResponseWithCode(err, 500) &&
 	err.body === 'No application found to associate with the api key';
-
-export const isUniqueKeyViolationResponse = ({ code, body }: ErrorResponse) =>
-	code === 'BalenaRequestError' &&
-	!!body &&
-	// api translated response
-	(body === 'Unique key constraint violated' ||
-		// pine response (tested on pine 10)
-		/^".*" must be unique\.$/.test(body));
 
 export const treatAsMissingApplication = (nameOrId: string | number) => (
 	err: Error,
@@ -278,7 +264,6 @@ export default {
 	isNotFoundResponse,
 	isNoDeviceForKeyResponse,
 	isNoApplicationForKeyResponse,
-	isUniqueKeyViolationResponse,
 	treatAsMissingApplication,
 	treatAsMissingDevice,
 	isDevelopmentVersion,
