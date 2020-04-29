0d60f9ce33469b0787dc7a0147c2669564ddf456
Thodoris Greasidis <thgreasi@gmail.com>
2020-04-29 20:08:54 +0300

    gulpfile: Convert to JavaScript
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- gulpfile.js
+++ gulpfile.js
@@ -1,74 +1,84 @@
-gulp = require('gulp')
-mocha = require('gulp-mocha')
-replace = require('gulp-replace')
-browserify = require('browserify')
-uglifyEs = require('uglify-es')
-uglifyComposer = require('gulp-uglify/composer')
-source = require('vinyl-source-stream')
-buffer = require('vinyl-buffer')
+const gulp = require('gulp');
+const gulpMocha = require('gulp-mocha');
+const replace = require('gulp-replace');
+const browserify = require('browserify');
+const uglifyEs = require('uglify-es');
+const uglifyComposer = require('gulp-uglify/composer');
+const source = require('vinyl-source-stream');
+const buffer = require('vinyl-buffer');
 
-minify = uglifyComposer(uglifyEs, console)
+const minify = uglifyComposer(uglifyEs, console);
 
-packageJSON = require('./package.json')
+const packageJSON = require('./package.json');
 
-{ loadEnv } = require('./tests/loadEnv')
+const { loadEnv } = require('./tests/loadEnv');
 
-OPTIONS =
-	config:
-		browserLibraryName: 'balena-sdk'
-	files:
-		tests: [ 'tests/**/*.spec.ts', 'tests/**/*.spec.coffee' ]
-		browserEntry: 'balena.js'
-		browserOutput: 'balena-browser.js'
-		browserMinifiedOutput: 'balena-browser.min.js'
-	directories:
-		doc: 'doc/'
-		build: 'build/'
+const OPTIONS = {
+	config: {
+		browserLibraryName: 'balena-sdk',
+	},
+	files: {
+		tests: ['tests/**/*.spec.ts', 'tests/**/*.spec.coffee'],
+		browserEntry: 'balena.js',
+		browserOutput: 'balena-browser.js',
+		browserMinifiedOutput: 'balena-browser.min.js',
+	},
+	directories: {
+		doc: 'doc/',
+		build: 'build/',
+	},
+};
 
-gulp.task 'test', (cb) ->
-	loadEnv()
+gulp.task('test', function (cb) {
+	loadEnv();
 
-	{ TEST_ONLY_ON_ENVIRONMENT } = process.env
-	if TEST_ONLY_ON_ENVIRONMENT && TEST_ONLY_ON_ENVIRONMENT != 'node'
-		console.log("TEST_ONLY_ON_ENVIRONMENT is set to #{TEST_ONLY_ON_ENVIRONMENT}")
-		console.log('Skipping node tests')
-		return cb()
+	const { TEST_ONLY_ON_ENVIRONMENT } = process.env;
+	if (TEST_ONLY_ON_ENVIRONMENT && TEST_ONLY_ON_ENVIRONMENT !== 'node') {
+		console.log(
+			`TEST_ONLY_ON_ENVIRONMENT is set to ${TEST_ONLY_ON_ENVIRONMENT}`,
+		);
+		console.log('Skipping node tests');
+		return cb();
+	}
 
-	gulp.src(OPTIONS.files.tests, read: false)
-		.pipe(mocha({
+	return gulp.src(OPTIONS.files.tests, { read: false }).pipe(
+		gulpMocha({
 			reporter: 'spec',
 			require: ['ts-node/register/transpile-only', 'coffeescript/register'],
 			timeout: 5 * 60 * 1000,
-			slow: 1000
-		}))
+			slow: 1000,
+		}),
+	);
+});
 
-gulp.task 'inject-version', ->
-	gulp.src(["#{OPTIONS.directories.build}util/sdk-version.js"])
+gulp.task('inject-version', () =>
+	gulp
+		.src([`${OPTIONS.directories.build}util/sdk-version.js`])
 		.pipe(replace('__REPLACE_WITH_PACKAGE_JSON_VERSION__', packageJSON.version))
-		.pipe(gulp.dest("#{OPTIONS.directories.build}util/"))
+		.pipe(gulp.dest(`${OPTIONS.directories.build}util/`)),
+);
 
-gulp.task 'pack-browser', ->
-	bundle = browserify
+gulp.task('pack-browser', function () {
+	const bundle = browserify({
 		entries: OPTIONS.files.browserEntry,
-		basedir: OPTIONS.directories.build
-		standalone: OPTIONS.config.browserLibraryName
-
-	# These modules are referenced in the code, but only get used in Node:
-	.exclude('fs')
-	.exclude('path')
-	.exclude('balena-settings-client')
-	.exclude('node-localstorage')
-	.bundle()
+		basedir: OPTIONS.directories.build,
+		standalone: OPTIONS.config.browserLibraryName,
+	})
+		.exclude('fs')
+		.exclude('path')
+		.exclude('balena-settings-client')
+		.exclude('node-localstorage')
+		.bundle();
 
 	bundle
 		.pipe(source(OPTIONS.files.browserOutput))
-		.pipe(gulp.dest(OPTIONS.directories.build))
+		.pipe(gulp.dest(OPTIONS.directories.build));
 
-	bundle
+	return bundle
 		.pipe(source(OPTIONS.files.browserMinifiedOutput))
 		.pipe(buffer())
 		.pipe(minify())
-		.pipe(gulp.dest(OPTIONS.directories.build))
+		.pipe(gulp.dest(OPTIONS.directories.build));
+});
 
-gulp.task 'build',
-	gulp.series(['pack-browser'])
+gulp.task('build', gulp.series(['pack-browser']));
