a55fee6e8ab8d44f2db0ead2558d51809a3f2aa8
Thodoris Greasidis <thgreasi@gmail.com>
2020-04-29 19:58:34 +0300

    karma.conf: Convert to JavaScript
    Change-type: patch
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- karma.conf.js
+++ karma.conf.js
@@ -1,51 +1,66 @@
-_ = require('lodash')
-getKarmaConfig = require('balena-config-karma')
-packageJSON = require('./package.json')
-{ loadEnv } = require('./tests/loadEnv')
+const _ = require('lodash');
+const getKarmaConfig = require('balena-config-karma');
+const packageJSON = require('./package.json');
+const { loadEnv } = require('./tests/loadEnv');
 
-getKarmaConfig.DEFAULT_WEBPACK_CONFIG.externals = fs: true
+getKarmaConfig.DEFAULT_WEBPACK_CONFIG.externals = { fs: true };
 
-BROWSER_BUNDLE = 'build/balena-browser.js'
+const BROWSER_BUNDLE = 'build/balena-browser.js';
 
-module.exports = (config) ->
-	loadEnv()
+module.exports = function (config) {
+	loadEnv();
 
-	karmaConfig = getKarmaConfig(packageJSON)
-	karmaConfig.plugins.push(require('karma-env-preprocessor'))
-	# do not pre-process the browser build
-	karmaConfig.preprocessors = _.omitBy(karmaConfig.preprocessors, (_value, key) -> _.startsWith(key, 'build/'))
-	karmaConfig.preprocessors['tests/**/*.coffee'] = [ 'webpack', 'sourcemap', 'env' ]
-	karmaConfig.client = mocha:
-		timeout: 5 * 60 * 1000
-		slow: 10 * 1000
+	const karmaConfig = getKarmaConfig(packageJSON);
+	karmaConfig.plugins.push(require('karma-env-preprocessor'));
+	// do not pre-process the browser build
+	karmaConfig.preprocessors = _.omitBy(
+		karmaConfig.preprocessors,
+		(_value, key) => _.startsWith(key, 'build/'),
+	);
+	karmaConfig.preprocessors['tests/**/*.coffee'] = [
+		'webpack',
+		'sourcemap',
+		'env',
+	];
+	karmaConfig.client = {
+		mocha: {
+			timeout: 5 * 60 * 1000,
+			slow: 10 * 1000,
+		},
+	};
 	karmaConfig.files = [
-		BROWSER_BUNDLE
-		'tests/**/*.spec.coffee'
-		'tests/**/*.spec.ts'
-	]
+		BROWSER_BUNDLE,
+		'tests/**/*.spec.coffee',
+		'tests/**/*.spec.ts',
+	];
 
-	{ TEST_ONLY_ON_ENVIRONMENT } = process.env
-	if TEST_ONLY_ON_ENVIRONMENT && TEST_ONLY_ON_ENVIRONMENT != 'browser'
-		console.log("TEST_ONLY_ON_ENVIRONMENT is set to #{TEST_ONLY_ON_ENVIRONMENT}")
-		console.log('Skipping browser tests')
-		karmaConfig.files = []
-		karmaConfig.failOnEmptyTestSuite = false
+	const { TEST_ONLY_ON_ENVIRONMENT } = process.env;
+	if (TEST_ONLY_ON_ENVIRONMENT && TEST_ONLY_ON_ENVIRONMENT !== 'browser') {
+		console.log(
+			`TEST_ONLY_ON_ENVIRONMENT is set to ${TEST_ONLY_ON_ENVIRONMENT}`,
+		);
+		console.log('Skipping browser tests');
+		karmaConfig.files = [];
+		karmaConfig.failOnEmptyTestSuite = false;
+	}
 
-	karmaConfig.browserConsoleLogOptions =
-		level: 'log'
-		format: '%b %T: %m'
-		terminal: true
+	karmaConfig.browserConsoleLogOptions = {
+		level: 'log',
+		format: '%b %T: %m',
+		terminal: true,
+	};
 
 	karmaConfig.envPreprocessor = [
-		'TEST_API_URL'
-		'TEST_EMAIL'
-		'TEST_PASSWORD'
-		'TEST_USERNAME'
-		'TEST_PAID_EMAIL'
-		'TEST_PAID_PASSWORD'
-		'TEST_REGISTER_EMAIL'
-		'TEST_REGISTER_PASSWORD'
-		'TEST_REGISTER_USERNAME'
-	]
+		'TEST_API_URL',
+		'TEST_EMAIL',
+		'TEST_PASSWORD',
+		'TEST_USERNAME',
+		'TEST_PAID_EMAIL',
+		'TEST_PAID_PASSWORD',
+		'TEST_REGISTER_EMAIL',
+		'TEST_REGISTER_PASSWORD',
+		'TEST_REGISTER_USERNAME',
+	];
 
-	config.set(karmaConfig)
+	return config.set(karmaConfig);
+};
