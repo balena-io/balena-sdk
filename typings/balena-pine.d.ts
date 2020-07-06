5398e0b09cd78a0025eef0fc58ca5578f81b6be3
Pagan Gazzard <page@balena.io>
2020-07-06 15:36:52 +0100

    Simplify balena-pine custom typings
    Change-type: patch

--- typings/balena-pine.d.ts
+++ typings/balena-pine.d.ts
@@ -1,23 +1,16 @@
 import * as Bluebird from 'bluebird';
 import * as PineClient from './pinejs-client-core';
 
-/* tslint:disable:no-namespace */
-declare namespace BalenaPine {
-	interface Pine {
-		delete<T>(
-			params: PineClient.ParamsObjWithId<T> | PineClient.ParamsObjWithFilter<T>,
-		): Bluebird<'OK'>;
-		get<T>(params: PineClient.ParamsObjWithId<T>): Bluebird<T>;
-		get<T>(params: PineClient.ParamsObj<T>): Bluebird<T[]>;
-		get<T, Result>(params: PineClient.ParamsObj<T>): Bluebird<Result>;
-		post<T>(params: PineClient.ParamsObj<T>): Bluebird<T>;
-		patch<T>(
-			params: PineClient.ParamsObjWithId<T> | PineClient.ParamsObjWithFilter<T>,
-		): Bluebird<'OK'>;
-		upsert<T>(params: PineClient.UpsertParams<T>): Bluebird<T | 'OK'>;
-	}
+export interface Pine {
+	delete<T>(
+		params: PineClient.ParamsObjWithId<T> | PineClient.ParamsObjWithFilter<T>,
+	): Bluebird<'OK'>;
+	get<T>(params: PineClient.ParamsObjWithId<T>): Bluebird<T>;
+	get<T>(params: PineClient.ParamsObj<T>): Bluebird<T[]>;
+	get<T, Result>(params: PineClient.ParamsObj<T>): Bluebird<Result>;
+	post<T>(params: PineClient.ParamsObj<T>): Bluebird<T>;
+	patch<T>(
+		params: PineClient.ParamsObjWithId<T> | PineClient.ParamsObjWithFilter<T>,
+	): Bluebird<'OK'>;
+	upsert<T>(params: PineClient.UpsertParams<T>): Bluebird<T | 'OK'>;
 }
-
-declare function BalenaPine(options: object): BalenaPine.Pine;
-
-export = BalenaPine;
