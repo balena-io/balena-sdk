c28af087f9b8e4ae805ee638438f4dd6116d0cdb
Pagan Gazzard <page@balena.io>
2020-07-06 21:34:16 +0100

    Improve internal typings by avoiding some `any` cases
    Change-type: patch

--- lib/models/api-key.ts
+++ lib/models/api-key.ts
@@ -27,156 +27,87 @@ const getApiKeysModel = function (
 ) {
 	const { pine, request } = deps;
 	const { apiUrl } = opts;
-	exports = {};
-
-	/**
-	 * @summary Creates a new user API key
-	 * @name create
-	 * @public
-	 * @function
-	 * @memberof balena.models.apiKey
-	 *
-	 * @description This method registers a new api key for the current user with the name given.
-	 *
-	 * @param {String} name - the API key name
-	 * @param {String} [description=null] - the API key description
-	 *
-	 * @fulfil {String} - API key
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.apiKey.create(apiKeyName).then(function(apiKey) {
-	 * 	console.log(apiKey);
-	 * });
-	 *
-	 * @example
-	 * balena.models.apiKey.create(apiKeyName, apiKeyDescription).then(function(apiKey) {
-	 * 	console.log(apiKey);
-	 * });
-	 *
-	 * @example
-	 * balena.models.apiKey.create(apiKeyName, function(error, apiKey) {
-	 * 	if (error) throw error;
-	 * 	console.log(apiKey);
-	 * });
-	 */
-	exports.create = function (
-		name: string,
-		description: string | null = null,
-	): Bluebird<string> {
-		const apiKeyBody: { name: string; description?: string | null } = {
-			name,
-		};
-		if (typeof description === 'string' && !!description) {
-			apiKeyBody.description = description;
-		}
-		return request
-			.send({
-				method: 'POST',
-				url: '/api-key/user/full',
-				baseUrl: apiUrl,
-				body: apiKeyBody,
-			})
-			.get('body')
-			.catch(function () {
-				throw new errors.BalenaNotLoggedIn();
-			});
-	};
-
-	/**
-	 * @summary Get all API keys
-	 * @name getAll
-	 * @public
-	 * @function
-	 * @memberof balena.models.apiKey
-	 *
-	 * @param {Object} [options={}] - extra pine options to use
-	 * @fulfil {Object[]} - apiKeys
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.apiKey.getAll().then(function(apiKeys) {
-	 * 	console.log(apiKeys);
-	 * });
-	 *
-	 * @example
-	 * balena.models.apiKey.getAll(function(error, apiKeys) {
-	 * 	if (error) throw error;
-	 * 	console.log(apiKeys);
-	 * });
-	 */
-	exports.getAll = function (
-		options: BalenaSdk.PineOptions<BalenaSdk.ApiKey> = {},
-	): Bluebird<BalenaSdk.ApiKey[]> {
-		return pine.get<BalenaSdk.ApiKey>({
-			resource: 'api_key',
-			options: mergePineOptions(
-				{
-					// the only way to reason whether
-					// it's a named user api key is whether
-					// it has a name
-					$filter: {
-						name: {
-							$ne: null,
-						},
-					},
-					$orderby: 'name asc',
-				},
-				options,
-			),
-		});
-	};
-
-	/**
-	 * @summary Update the details of an API key
-	 * @name update
-	 * @public
-	 * @function
-	 * @memberof balena.models.apiKey
-	 *
-	 * @param {Number} id - API key id
-	 * @param {Object} apiKeyInfo - an object with the updated name or description
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.apiKey.update(123, { name: 'updatedName' });
-	 *
-	 * @example
-	 * balena.models.apiKey.update(123, { description: 'updated description' });
-	 *
-	 * @example
-	 * balena.models.apiKey.update(123, { name: 'updatedName', description: 'updated description' });
-	 *
-	 * @example
-	 * balena.models.apiKey.update(123, { name: 'updatedName', description: 'updated description' }, function(error, apiKeys) {
-	 * 	if (error) throw error;
-	 * 	console.log(apiKeys);
-	 * });
-	 */
-	exports.update = function (
-		id: number,
-		apiKeyInfo: { name?: string; description?: string },
-	): Bluebird<void> {
-		return Bluebird.try<void>(() => {
-			if (!apiKeyInfo) {
-				throw new errors.BalenaInvalidParameterError('apiKeyInfo', apiKeyInfo);
-			}
-			if (apiKeyInfo.name === null || apiKeyInfo.name === '') {
-				throw new errors.BalenaInvalidParameterError(
-					'apiKeyInfo.name',
-					apiKeyInfo.name,
-				);
-			}
-			const body = {
-				name: apiKeyInfo.name,
-				description: apiKeyInfo.description,
+	const exports = {
+		/**
+		 * @summary Creates a new user API key
+		 * @name create
+		 * @public
+		 * @function
+		 * @memberof balena.models.apiKey
+		 *
+		 * @description This method registers a new api key for the current user with the name given.
+		 *
+		 * @param {String} name - the API key name
+		 * @param {String} [description=null] - the API key description
+		 *
+		 * @fulfil {String} - API key
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.apiKey.create(apiKeyName).then(function(apiKey) {
+		 * 	console.log(apiKey);
+		 * });
+		 *
+		 * @example
+		 * balena.models.apiKey.create(apiKeyName, apiKeyDescription).then(function(apiKey) {
+		 * 	console.log(apiKey);
+		 * });
+		 *
+		 * @example
+		 * balena.models.apiKey.create(apiKeyName, function(error, apiKey) {
+		 * 	if (error) throw error;
+		 * 	console.log(apiKey);
+		 * });
+		 */
+		create(name: string, description: string | null = null): Bluebird<string> {
+			const apiKeyBody: { name: string; description?: string | null } = {
+				name,
 			};
-			return pine
-				.patch<BalenaSdk.ApiKey>({
-					resource: 'api_key',
-					id,
-					body,
-					options: {
+			if (typeof description === 'string' && !!description) {
+				apiKeyBody.description = description;
+			}
+			return request
+				.send({
+					method: 'POST',
+					url: '/api-key/user/full',
+					baseUrl: apiUrl,
+					body: apiKeyBody,
+				})
+				.get('body')
+				.catch(function () {
+					throw new errors.BalenaNotLoggedIn();
+				});
+		},
+
+		/**
+		 * @summary Get all API keys
+		 * @name getAll
+		 * @public
+		 * @function
+		 * @memberof balena.models.apiKey
+		 *
+		 * @param {Object} [options={}] - extra pine options to use
+		 * @fulfil {Object[]} - apiKeys
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.apiKey.getAll().then(function(apiKeys) {
+		 * 	console.log(apiKeys);
+		 * });
+		 *
+		 * @example
+		 * balena.models.apiKey.getAll(function(error, apiKeys) {
+		 * 	if (error) throw error;
+		 * 	console.log(apiKeys);
+		 * });
+		 */
+		getAll(
+			options: BalenaSdk.PineOptions<BalenaSdk.ApiKey> = {},
+		): Bluebird<BalenaSdk.ApiKey[]> {
+			return pine.get<BalenaSdk.ApiKey>({
+				resource: 'api_key',
+				options: mergePineOptions(
+					{
 						// the only way to reason whether
 						// it's a named user api key is whether
 						// it has a name
@@ -185,46 +116,115 @@ const getApiKeysModel = function (
 								$ne: null,
 							},
 						},
+						$orderby: 'name asc',
+					},
+					options,
+				),
+			});
+		},
+
+		/**
+		 * @summary Update the details of an API key
+		 * @name update
+		 * @public
+		 * @function
+		 * @memberof balena.models.apiKey
+		 *
+		 * @param {Number} id - API key id
+		 * @param {Object} apiKeyInfo - an object with the updated name or description
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.apiKey.update(123, { name: 'updatedName' });
+		 *
+		 * @example
+		 * balena.models.apiKey.update(123, { description: 'updated description' });
+		 *
+		 * @example
+		 * balena.models.apiKey.update(123, { name: 'updatedName', description: 'updated description' });
+		 *
+		 * @example
+		 * balena.models.apiKey.update(123, { name: 'updatedName', description: 'updated description' }, function(error, apiKeys) {
+		 * 	if (error) throw error;
+		 * 	console.log(apiKeys);
+		 * });
+		 */
+		update(
+			id: number,
+			apiKeyInfo: { name?: string; description?: string },
+		): Bluebird<void> {
+			return Bluebird.try<void>(() => {
+				if (!apiKeyInfo) {
+					throw new errors.BalenaInvalidParameterError(
+						'apiKeyInfo',
+						apiKeyInfo,
+					);
+				}
+				if (apiKeyInfo.name === null || apiKeyInfo.name === '') {
+					throw new errors.BalenaInvalidParameterError(
+						'apiKeyInfo.name',
+						apiKeyInfo.name,
+					);
+				}
+				const body = {
+					name: apiKeyInfo.name,
+					description: apiKeyInfo.description,
+				};
+				return pine
+					.patch<BalenaSdk.ApiKey>({
+						resource: 'api_key',
+						id,
+						body,
+						options: {
+							// the only way to reason whether
+							// it's a named user api key is whether
+							// it has a name
+							$filter: {
+								name: {
+									$ne: null,
+								},
+							},
+						},
+					})
+					.return();
+			});
+		},
+
+		/**
+		 * @summary Revoke an API key
+		 * @name revoke
+		 * @public
+		 * @function
+		 * @memberof balena.models.apiKey
+		 *
+		 * @param {Number} id - API key id
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.apiKey.revoke(123);
+		 *
+		 * @example
+		 * balena.models.apiKey.revoke(123, function(error) {
+		 * 	if (error) throw error;
+		 * });
+		 */
+		revoke(id: number): Bluebird<void> {
+			return pine
+				.delete({
+					resource: 'api_key',
+					id,
+					options: {
+						// so that we don't accidentally delete
+						// a non named user api key
+						$filter: {
+							name: {
+								$ne: null,
+							},
+						},
 					},
 				})
 				.return();
-		});
-	};
-
-	/**
-	 * @summary Revoke an API key
-	 * @name revoke
-	 * @public
-	 * @function
-	 * @memberof balena.models.apiKey
-	 *
-	 * @param {Number} id - API key id
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.apiKey.revoke(123);
-	 *
-	 * @example
-	 * balena.models.apiKey.revoke(123, function(error) {
-	 * 	if (error) throw error;
-	 * });
-	 */
-	exports.revoke = function (id: number): Bluebird<void> {
-		return pine
-			.delete({
-				resource: 'api_key',
-				id,
-				options: {
-					// so that we don't accidentally delete
-					// a non named user api key
-					$filter: {
-						name: {
-							$ne: null,
-						},
-					},
-				},
-			})
-			.return();
+		},
 	};
 
 	return exports;
--- lib/models/billing.ts
+++ lib/models/billing.ts
@@ -32,205 +32,206 @@ const getBillingModel = function (
 	const { request } = deps;
 	const { apiUrl, isBrowser } = opts;
 
-	exports = {};
-	/**
-	 * @summary Get the user's billing account
-	 * @name getAccount
-	 * @public
-	 * @function
-	 * @memberof balena.models.billing
-	 *
-	 * @fulfil {Object} - billing account
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.billing.getAccount().then(function(billingAccount) {
-	 * 	console.log(billingAccount);
-	 * });
-	 *
-	 * @example
-	 * balena.models.billing.getAccount(function(error, billingAccount) {
-	 * 	if (error) throw error;
-	 * 	console.log(billingAccount);
-	 * });
-	 */
-	exports.getAccount = (): Bluebird<BillingAccountInfo> =>
-		request
-			.send({
-				method: 'GET',
-				url: '/user/billing/account',
-				baseUrl: apiUrl,
-			})
-			.get('body');
+	const exports = {
+		/**
+		 * @summary Get the user's billing account
+		 * @name getAccount
+		 * @public
+		 * @function
+		 * @memberof balena.models.billing
+		 *
+		 * @fulfil {Object} - billing account
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.billing.getAccount().then(function(billingAccount) {
+		 * 	console.log(billingAccount);
+		 * });
+		 *
+		 * @example
+		 * balena.models.billing.getAccount(function(error, billingAccount) {
+		 * 	if (error) throw error;
+		 * 	console.log(billingAccount);
+		 * });
+		 */
+		getAccount: (): Bluebird<BillingAccountInfo> =>
+			request
+				.send({
+					method: 'GET',
+					url: '/user/billing/account',
+					baseUrl: apiUrl,
+				})
+				.get('body'),
 
-	/**
-	 * @summary Get the current billing plan
-	 * @name getPlan
-	 * @public
-	 * @function
-	 * @memberof balena.models.billing
-	 *
-	 * @fulfil {Object} - billing plan
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.billing.getPlan().then(function(billingPlan) {
-	 * 	console.log(billingPlan);
-	 * });
-	 *
-	 * @example
-	 * balena.models.billing.getPlan(function(error, billingPlan) {
-	 * 	if (error) throw error;
-	 * 	console.log(billingPlan);
-	 * });
-	 */
-	exports.getPlan = (): Bluebird<BillingPlanInfo> =>
-		request
-			.send({
-				method: 'GET',
-				url: '/user/billing/plan',
-				baseUrl: apiUrl,
-			})
-			.get('body');
+		/**
+		 * @summary Get the current billing plan
+		 * @name getPlan
+		 * @public
+		 * @function
+		 * @memberof balena.models.billing
+		 *
+		 * @fulfil {Object} - billing plan
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.billing.getPlan().then(function(billingPlan) {
+		 * 	console.log(billingPlan);
+		 * });
+		 *
+		 * @example
+		 * balena.models.billing.getPlan(function(error, billingPlan) {
+		 * 	if (error) throw error;
+		 * 	console.log(billingPlan);
+		 * });
+		 */
+		getPlan: (): Bluebird<BillingPlanInfo> =>
+			request
+				.send({
+					method: 'GET',
+					url: '/user/billing/plan',
+					baseUrl: apiUrl,
+				})
+				.get('body'),
 
-	/**
-	 * @summary Get the current billing information
-	 * @name getBillingInfo
-	 * @public
-	 * @function
-	 * @memberof balena.models.billing
-	 *
-	 * @fulfil {Object} - billing information
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.billing.getBillingInfo().then(function(billingInfo) {
-	 * 	console.log(billingInfo);
-	 * });
-	 *
-	 * @example
-	 * balena.models.billing.getBillingInfo(function(error, billingInfo) {
-	 * 	if (error) throw error;
-	 * 	console.log(billingInfo);
-	 * });
-	 */
-	exports.getBillingInfo = (): Bluebird<BillingInfo> =>
-		request
-			.send({
-				method: 'GET',
-				url: '/user/billing/info',
-				baseUrl: apiUrl,
-			})
-			.get('body');
+		/**
+		 * @summary Get the current billing information
+		 * @name getBillingInfo
+		 * @public
+		 * @function
+		 * @memberof balena.models.billing
+		 *
+		 * @fulfil {Object} - billing information
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.billing.getBillingInfo().then(function(billingInfo) {
+		 * 	console.log(billingInfo);
+		 * });
+		 *
+		 * @example
+		 * balena.models.billing.getBillingInfo(function(error, billingInfo) {
+		 * 	if (error) throw error;
+		 * 	console.log(billingInfo);
+		 * });
+		 */
+		getBillingInfo: (): Bluebird<BillingInfo> =>
+			request
+				.send({
+					method: 'GET',
+					url: '/user/billing/info',
+					baseUrl: apiUrl,
+				})
+				.get('body'),
 
-	/**
-	 * @summary Update the current billing information
-	 * @name updateBillingInfo
-	 * @public
-	 * @function
-	 * @memberof balena.models.billing
-	 *
-	 * @param {Object} billingInfo - an object containing a billing info token_id
-	 * @param {String} billingInfo.token_id - the token id generated for the billing info form
-	 * @param {(String|undefined)} [billingInfo.'g-recaptcha-response'] - the captcha response
-	 * @fulfil {Object} - billing information
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.billing.updateBillingInfo({ token_id: 'xxxxxxx' }).then(function(billingInfo) {
-	 * 	console.log(billingInfo);
-	 * });
-	 *
-	 * @example
-	 * balena.models.billing.updateBillingInfo({ token_id: 'xxxxxxx' }, function(error, billingInfo) {
-	 * 	if (error) throw error;
-	 * 	console.log(billingInfo);
-	 * });
-	 */
-	exports.updateBillingInfo = (
-		billingInfo: TokenBillingSubmitInfo,
-	): Bluebird<BillingInfo> =>
-		request
-			.send({
-				method: 'POST',
-				url: '/user/billing/info',
-				baseUrl: apiUrl,
-				body: billingInfo,
-			})
-			.get('body');
+		/**
+		 * @summary Update the current billing information
+		 * @name updateBillingInfo
+		 * @public
+		 * @function
+		 * @memberof balena.models.billing
+		 *
+		 * @param {Object} billingInfo - an object containing a billing info token_id
+		 * @param {String} billingInfo.token_id - the token id generated for the billing info form
+		 * @param {(String|undefined)} [billingInfo.'g-recaptcha-response'] - the captcha response
+		 * @fulfil {Object} - billing information
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.billing.updateBillingInfo({ token_id: 'xxxxxxx' }).then(function(billingInfo) {
+		 * 	console.log(billingInfo);
+		 * });
+		 *
+		 * @example
+		 * balena.models.billing.updateBillingInfo({ token_id: 'xxxxxxx' }, function(error, billingInfo) {
+		 * 	if (error) throw error;
+		 * 	console.log(billingInfo);
+		 * });
+		 */
+		updateBillingInfo: (
+			billingInfo: TokenBillingSubmitInfo,
+		): Bluebird<BillingInfo> =>
+			request
+				.send({
+					method: 'POST',
+					url: '/user/billing/info',
+					baseUrl: apiUrl,
+					body: billingInfo,
+				})
+				.get('body'),
 
-	/**
-	 * @summary Get the available invoices
-	 * @name getInvoices
-	 * @public
-	 * @function
-	 * @memberof balena.models.billing
-	 *
-	 * @fulfil {Object} - invoices
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * balena.models.billing.getInvoices().then(function(invoices) {
-	 * 	console.log(invoices);
-	 * });
-	 *
-	 * @example
-	 * balena.models.billing.getInvoices(function(error, invoices) {
-	 * 	if (error) throw error;
-	 * 	console.log(invoices);
-	 * });
-	 */
-	exports.getInvoices = (): Bluebird<InvoiceInfo[]> =>
-		request
-			.send({
-				method: 'GET',
-				url: '/user/billing/invoices',
-				baseUrl: apiUrl,
-			})
-			.get('body');
+		/**
+		 * @summary Get the available invoices
+		 * @name getInvoices
+		 * @public
+		 * @function
+		 * @memberof balena.models.billing
+		 *
+		 * @fulfil {Object} - invoices
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * balena.models.billing.getInvoices().then(function(invoices) {
+		 * 	console.log(invoices);
+		 * });
+		 *
+		 * @example
+		 * balena.models.billing.getInvoices(function(error, invoices) {
+		 * 	if (error) throw error;
+		 * 	console.log(invoices);
+		 * });
+		 */
+		getInvoices: (): Bluebird<InvoiceInfo[]> =>
+			request
+				.send({
+					method: 'GET',
+					url: '/user/billing/invoices',
+					baseUrl: apiUrl,
+				})
+				.get('body'),
 
-	/**
-	 * @summary Download a specific invoice
-	 * @name downloadInvoice
-	 * @public
-	 * @function
-	 * @memberof balena.models.billing
-	 *
-	 * @param {String} - an invoice number
-	 * @fulfil {Blob|ReadableStream} - blob on the browser, download stream on node
-	 * @returns {Bluebird}
-	 *
-	 * @example
-	 * # Browser
-	 * balena.models.billing.downloadInvoice('0000').then(function(blob) {
-	 * 	console.log(blob);
-	 * });
-	 * # Node
-	 * balena.models.billing.downloadInvoice('0000').then(function(stream) {
-	 * 	stream.pipe(fs.createWriteStream('foo/bar/invoice-0000.pdf'));
-	 * });
-	 */
-	exports.downloadInvoice = function (
-		invoiceNumber: string,
-	): Bluebird<Blob | BalenaRequestStreamResult> {
-		const url = `/user/billing/invoices/${invoiceNumber}/download`;
+		/**
+		 * @summary Download a specific invoice
+		 * @name downloadInvoice
+		 * @public
+		 * @function
+		 * @memberof balena.models.billing
+		 *
+		 * @param {String} - an invoice number
+		 * @fulfil {Blob|ReadableStream} - blob on the browser, download stream on node
+		 * @returns {Bluebird}
+		 *
+		 * @example
+		 * # Browser
+		 * balena.models.billing.downloadInvoice('0000').then(function(blob) {
+		 * 	console.log(blob);
+		 * });
+		 * # Node
+		 * balena.models.billing.downloadInvoice('0000').then(function(stream) {
+		 * 	stream.pipe(fs.createWriteStream('foo/bar/invoice-0000.pdf'));
+		 * });
+		 */
+		downloadInvoice(
+			invoiceNumber: string,
+		): Bluebird<Blob | BalenaRequestStreamResult> {
+			const url = `/user/billing/invoices/${invoiceNumber}/download`;
 
-		if (!isBrowser) {
-			return request.stream({
-				method: 'GET',
-				url,
-				baseUrl: apiUrl,
-			});
-		}
+			if (!isBrowser) {
+				return request.stream({
+					method: 'GET',
+					url,
+					baseUrl: apiUrl,
+				});
+			}
 
-		return request
-			.send({
-				method: 'GET',
-				url,
-				baseUrl: apiUrl,
-				responseFormat: 'blob',
-			})
-			.get('body');
+			return request
+				.send({
+					method: 'GET',
+					url,
+					baseUrl: apiUrl,
+					responseFormat: 'blob',
+				})
+				.get('body');
+		},
 	};
 
 	return exports;
