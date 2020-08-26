f5fed0e20b94fa32eb1b26e3a06659756c83a89e
Thodoris Greasidis <thgreasi@gmail.com>
2020-08-26 18:38:43 +0300

    Add support for bundling with pkg
    Connects-to: #973
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 var esVersion = require('@balena/es-version').get(['es2015', 'es2018']);
-module.exports = require('./' + esVersion);
+module.exports = typeof window !== 'undefined' && window !== null ?
+	require('./' + esVersion) :
+	// support bundling with pkg
+	require(require('path').join(__dirname, esVersion));
