609417d1519dbd5466eea789e884ea4cbd8bf4fe
Thodoris Greasidis <thgreasi@gmail.com>
2021-08-27 15:20:24 +0300

    models.hostapp: Add a getAvailableOsVersions() convenience method
    Change-type: minor
    Signed-off-by: Thodoris Greasidis <thodoris@balena.io>

--- tests/integration/models/hostapp.spec.ts
+++ tests/integration/models/hostapp.spec.ts
@@ -75,7 +75,7 @@ describe('Hostapp model', function () {
 			expect(firstRes).to.equal(secondRes);
 		});
 
-		it('should cache the results when providing extra options', async () => {
+		it('should not cache the results when providing extra options', async () => {
 			const firstRes = await balena.models.hostapp.getAllOsVersions(
 				['raspberrypi3'],
 				{ $filter: { is_invalidated: false } },
@@ -85,6 +85,67 @@ describe('Hostapp model', function () {
 				{ $filter: { is_invalidated: false } },
 			);
 			expect(firstRes).to.not.equal(secondRes);
+		});
+	});
+
+	parallel('balena.models.hostapp.getAvailableOsVersions()', function () {
+		it("should contain both balenaOS and balenaOS ESR OS types'", async () => {
+			const res = await balena.models.hostapp.getAvailableOsVersions([
+				'fincm3',
+				'raspberrypi3',
+			]);
+
+			containsVersion(res['fincm3'], {
+				strippedVersion: '2.29.0+rev1',
+				variant: 'dev',
+			});
+			containsVersion(res['fincm3'], {
+				strippedVersion: '2.29.0+rev1',
+				variant: 'prod',
+			});
+			containsVersion(res['fincm3'], {
+				strippedVersion: '2020.04.0',
+				variant: 'dev',
+			});
+			containsVersion(res['fincm3'], {
+				strippedVersion: '2020.04.0',
+				variant: 'prod',
+			});
+
+			containsVersion(res['raspberrypi3'], {
+				strippedVersion: '2.29.0+rev1',
+				variant: 'dev',
+			});
+			containsVersion(res['raspberrypi3'], {
+				strippedVersion: '2.29.0+rev1',
+				variant: 'prod',
+			});
+			containsVersion(res['raspberrypi3'], {
+				strippedVersion: '2020.04.0',
+				variant: 'dev',
+			});
+			containsVersion(res['raspberrypi3'], {
+				strippedVersion: '2020.04.0',
+				variant: 'prod',
+			});
+		});
+
+		it('should return an empty object for non-existent DTs', async () => {
+			const res = await balena.models.hostapp.getAvailableOsVersions([
+				'blahbleh',
+			]);
+
+			expect(res).to.deep.equal({});
+		});
+
+		it('should cache the results', async () => {
+			const firstRes = await balena.models.hostapp.getAvailableOsVersions([
+				'raspberrypi3',
+			]);
+			const secondRes = await balena.models.hostapp.getAvailableOsVersions([
+				'raspberrypi3',
+			]);
+			expect(firstRes).to.equal(secondRes);
 		});
 	});
 });
