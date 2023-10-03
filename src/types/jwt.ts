59fd11688780c2b703d036715842ad0615ed8ee1
Thodoris Greasidis <thgreasi@gmail.com>
2023-10-03 16:37:37 +0300

    jwt: Deprecate the profile fields in favor of the user_profile
    Change-type: patch

--- src/types/jwt.ts
+++ src/types/jwt.ts
@@ -15,14 +15,23 @@ export interface JWTUser {
 	/** @deprecated */
 	features?: string[];
 
+	/** @deprecated Use the user_profile resource */
 	first_name?: string;
+	/** @deprecated Use the user_profile resource */
 	last_name?: string;
+	/** @deprecated Use the user_profile resource */
 	email?: string;
+	/** @deprecated Use the user_profile resource */
 	account_type?: string;
+	/** @deprecated Use the user_profile resource */
 	company?: string;
+	/** @deprecated Use the user_profile resource */
 	has_disabled_newsletter?: boolean;
+	/** @deprecated Use the user_profile resource */
 	hasPasswordSet?: boolean;
+	/** @deprecated Use the user_profile resource */
 	must_be_verified?: boolean;
+	/** @deprecated Use the user_profile resource */
 	is_verified?: boolean;
 
 	/** @deprecated */
