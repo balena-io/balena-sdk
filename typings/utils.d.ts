0bba30a51f59168ea4f710f81cedbeac39ab59c1
Thodoris Greasidis <thgreasi@gmail.com>
2024-07-08 11:13:03 +0300

    Bump minimum supported Typescript version to v5.5.2
    Change-type: major
    Signed-off-by: Thodoris Greasidis

--- typings/utils.d.ts
+++ typings/utils.d.ts
@@ -37,8 +37,3 @@ export type AtLeast<T, K extends keyof T> = Pick<T, K> & Partial<Omit<T, K>>;
 export type ExactlyExtends<T, ExtendsBase> = ExtendsBase extends T
 	? T
 	: ExtendsBase;
-
-// TODO: Replace this workaround once TS adds support for this use case.
-// See: https://github.com/microsoft/TypeScript/issues/14829#issuecomment-322267089
-// See: https://github.com/millsp/ts-toolbelt/blob/3859d1819021800b96ed815abf5c300eb7b8f926/src/Function/NoInfer.ts#L27
-export type NoInfer<A> = [A][A extends any ? 0 : never];
