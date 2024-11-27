3fadd1b5262f4f6993f1a05d23403e6baa155e82
myarmolinsky <yarmolinskymatthew@gmail.com>
2024-11-27 08:00:05 -0500

    Drop deprecated property `contains__image` from `Release`
    Change-type: major

--- typing_tests/pine-options.ts
+++ typing_tests/pine-options.ts
@@ -586,7 +586,6 @@ export const appOptionsEValid20: BalenaSdk.PineOptions<BalenaSdk.Application> =
 type ReleaseExpandablePropsExpectation =
 	| 'is_created_by__user'
 	| 'belongs_to__application'
-	| 'contains__image'
 	| 'release_image'
 	| 'should_be_running_on__application'
 	| 'should_operate__device'
